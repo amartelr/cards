fe83c18db1cedb6e491eabc843ca2df96d288d5b
amartelr <alfred.martel@gmail.com>
2021-05-23 21:45:44 +0100

    Adding saveToFile methods to Deck

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	cards := newDeck()
+	cards.saveToFile("my_cards")
+
+	fmt.Println(cards.toString())
 	hand, remainingCards := deal(cards, 5)
 
 	hand.print()
