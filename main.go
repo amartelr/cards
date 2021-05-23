b86734ab9d92107a97103f0812905eb06f6073d0
amartelr <alfred.martel@gmail.com>
2021-05-23 14:21:45 +0100

    Slides

--- main.go
+++ main.go
@@ -3,7 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	card := "Ace of Spades"
 
-	fmt.Println(card)
+	cards := []string{"Aces of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
